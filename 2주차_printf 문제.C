e735a4ff4a11f97a17360ac0b71d484a7a68ee6a
AI-WonYJ <101448204+AI-WonYJ@users.noreply.github.com>
2022-07-17 23:44:59 +0900

    Create 2주차_printf 문제.C
    2주차 강의안 prinf 문제 풀이

---
+++ 2주차_printf 문제.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int value = 10;
+  printf("처음 변수값 = %d\n", value);
+  printf("10을 더한 값 = %d\n", value+10);
+  printf("10을 뺀 값 = %d\n", value-10);
+  printf("10을 곱한 값 = %d\n", value*10);
+  printf("10으로 나눈 값 = %d\n", value/10);
+}
