ba33bb254eedf740a3db258f613f117ad3f8764f
WonYoungJin <yjason75870421@gmail.com>
2022-07-17 23:26:58 +0900

    Create 2주차_scanf 문제.c
    2주차 강의안 scanf 문제

---
+++ 2주차_scanf 문제.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	// 巩力 1.
+	int value1;
+	int value2;
+	scanf("%d" ,&value1);
+	scanf("%d", &value2);
+	printf("%d + %d = %d\n", value1, value2,value1+value2);
+	printf("%d - %d = %d\n", value1, value2, value1 - value2);
+	return 0;
+
+	//巩力 2.
+
+	//巩力 3.
+
+	//巩力 4.
+}
