1c43e8fa2cdb01f8485c16f1145eb265b0ab98e4
AI-WonYJ <101448204+AI-WonYJ@users.noreply.github.com>
2022-07-18 00:04:40 +0900

    Update 2주차_scanf 문제.c

--- 2주차_scanf 문제.c
+++ 2주차_scanf 문제.c
@@ -13,6 +13,10 @@ int main() {
 	return 0;
 
 	// 문제 2.
+	int mom_age, dad_age, sis_age, my_age, bro_age;
+	printf("엄마, 아빠, 누나, 본인, 동생 나이를 차례로 입력하세요.\n");
+	scanf("%d %d %d %d %d",&mom_age, &dad_age, &sis_age, &my_age, &bro_age);
+	printf("가족 나이의 총합은 %d 세 입니다.", mom_age + dad_age + sis_age + my_age + bro_age);
 
 	// 문제 3.
 
