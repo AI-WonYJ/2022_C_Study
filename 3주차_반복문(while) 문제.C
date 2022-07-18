fde6860d9b5b659759c1f8752aad5ea8b34ba519
AI-WonYJ <101448204+AI-WonYJ@users.noreply.github.com>
2022-07-18 14:02:00 +0900

    Create 3주차_반복문(while) 문제.C
    3주차 강의안 while 반복문 문제 풀이

---
+++ 3주차_반복문(while) 문제.C
@@ -0,0 +1,63 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  // 문제 1.
+  int i = 1;
+  while (i <= 9) {
+    printf("7 * %d = %d\n", i, 7*i);
+    i++
+  }
+  
+  // 문제 2.
+  int j = 1
+  int set_num;
+  scanf("%d", &set_num);
+  while (j <= 9) {
+    printf("%d * j = %d\n", set_num, j, set_num*j);
+    j++
+  }
+  
+  // 문제 3.
+  int k = 10;
+  int sum = 0;
+  while (k <= 20) {
+    sum += k;
+    k++
+  }
+  printf("10부터 20까지의 합은 %d 입니다.\n", sum);
+  
+  // 문제 4.
+  int input_num;
+  scanf("%d", &input_num);
+  int o = 1;
+  int input_sum = 0;
+  while (o <= input_num) {
+    input_sum += o
+    o++
+  }
+  printf("1부터 %d까지의 합은 %d 입니다.\n", input_num, input_sum);
+  
+  // 문제 5.
+  int input_a, input_b;
+  scanf("%d %d", &input_a, &input_b);
+  input_ab_sum = 0;
+  input_cal = input_a;
+  while (input_cal <= input_b) {
+    input_ab_sum += input_cal;
+    input_cal++
+  }
+  pirntf("%d부터 %d까지의 합은 %d입니다.\n", input_a, input_b, input_ab_sum);
+  
+  // 문제 6.
+  int divive;
+  scanf("%d", &divive);
+  int u = 10;
+  while (0 < divive) {
+    int divive_num = divive/10;
+    printf("%d", divive%u);
+  }
+   
+  return 0;
+}
