ce4a6af8d0bdbd2c006347f9b00908696ae9d3f0
AI-WonYJ <101448204+AI-WonYJ@users.noreply.github.com>
2022-07-20 11:29:51 +0900

    Update 5주차_중첩 반복문을 활용한 알고리즘.C

--- 5주차_중첩 반복문을 활용한 알고리즘.C
+++ 5주차_중첩 반복문을 활용한 알고리즘.C
@@ -16,7 +16,7 @@ int main() {
   // 문제 2.
   int scan_star, j, j2;
   scanf("%d", &scan_star);
-  for(j = 1; j <= scan_star; j++;) {
+  for(j = 1; j <= scan_star; j++) {
     for(j2 = 1; j2 <= j; j2++) {
       printf("*");
     }
@@ -29,14 +29,15 @@ int main() {
     for(k2 = 1; k2 <= k; k2++; {
       printf(" ");
     }
-    for(k3 = 1; k3 <= 5-k; k3++;) {
+    for(k3 = 1; k3 <= 5-k; k3++) {
       printf("*");
     }
-    printf("\n")'
+    printf("\n");
   }
   
   // 문제 4.
   int scan_di, x;
+  int divive = 0;
   scnaf("%d", &scan_di);
   while(scan_di <= 0) {
     divive = scan_di % 10;
