b294315c1a0548a40592b7753042fb7036c552c2
AI-WonYJ <101448204+AI-WonYJ@users.noreply.github.com>
2022-07-20 11:26:52 +0900

    Update 5주차_기초 제어문을 활용한 알고리즘.C

--- 5주차_기초 제어문을 활용한 알고리즘.C
+++ 5주차_기초 제어문을 활용한 알고리즘.C
@@ -3,98 +3,101 @@
 #include <stdlib.h>
 
 int main() {
-  
-            // 약수 찾기
-  // 문제 1.
-  int measure_num;
-  for(int measure = 1; measure <= measure_num; measure++) {
-    if (measure_num % measure == 0) {
-      printf("%d\t",measure);
+
+    // 약수 찾기
+// 문제 1.
+    int measure_num;
+    scanf("%d", &measure_num);
+    for (int measure = 1; measure <= measure_num; measure++) {
+        if (measure_num % measure == 0) {
+            printf("%d  ", measure);
+        }
     }
-  }
-  
-  // 문제 2.
-  int measure_num2;
-  int measure2_cnt = 0;
-  for(int measure2 = 1; measure2 <= measure_num2; measure2++) {
-    if (measure_num2 % measure2 == 0) {
-      measure2_cnt++
+    printf("");
+
+    // 문제 2.
+    int measure_num2;
+    int measure2_cnt = 0;
+    scanf("%d", &measure_num2);
+    for (int measure2 = 1; measure2 <= measure_num2; measure2++) {
+        if (measure_num2 % measure2 == 0) {
+            measure2_cnt++;
+        }
     }
-  }
-  printf("%d",measure2_cnt);
-  
-            // 소수 판별하기
-  // 문제 3.
-  int prime_cnt;
-  for(int i = 1; i <= 503159; i++) {
-    if (503159 % i == 0) {
-      prime_cnt++;
+    printf("%d\n", measure2_cnt);
+
+    // 소수 판별하기
+// 문제 3.
+    int prime_cnt = 0;
+        for (int i = 1; i <= 503159; i++) {
+            if (503159 % i == 0) {
+                prime_cnt++;
+            }
+        }
+    if (prime_cnt == 2) {
+        printf("소수입니다.\n");
     }
-  }
-  if (prime_cnt == 2) {
-    printf("소수입니다.\n");
-  }
-  else {
-    printf("소수가 아닙니다.\n");
-  }
-  
-  // 문제 4.
-  int prime_cnt2;
-  for(int i2 = 1; i2 <= 503158; i2++) {
-    if (503158 % i2 == 0) {
-      prime_cnt2++;
+    else {
+        printf("소수가 아닙니다.\n");
     }
-  }
-  if (prime_cnt2 == 2) {
-    printf("소수입니다.\n");
-  }
-  else {
-    printf("소수가 아닙니다.\n");
-  }
-  
-  
-  // 문제 5.
-  int scan_prime;
-  printf("숫자 하나를 입력하세요.\n");
-  scanf("%d",&scan_prime);
-  int scan_prime_cnt;
-   for(int i3 = 1; i3 <= scan_prime; i3++) {
-    if (503158 % i3 == 0) {
-      scan_prime_cnt++;
+
+    // 문제 4.
+    int prime_cnt2 = 0;
+        for (int i2 = 1; i2 <= 503158; i2++) {
+            if (503158 % i2 == 0) {
+                prime_cnt2++;
+            }
+        }
+    if (prime_cnt2 == 2) {
+        printf("소수입니다.\n");
     }
-  }
-  if (scan_prime_cnt == 2) {
-    printf("%d은(는) 소수입니다.\n", scan_prime);
-  }
-  else {
-    printf("%d은(는) 소수가 아닙니다.\n", scan_prime);
-  }
-  
-          // 공약수 모두 찾기
-  //문제 6.
-  int a, b;
-  scanf("%d %d", &a, &b);
-  for (int x = 1; x <= a; x++) {
-    if (a % x == 0 && b % x == 0) {
-      printf("%d\n", x);
+    else {
+        printf("소수가 아닙니다.\n");
     }
-  }
-  
-          // 최대공약수 찾기
-  //문제 7.
-  int a2, b2;
-  int gcd;
-  printf("첫번째 수를 입력하세요\n");
-  scanf("%d", &a2);
-  printf("두번째 수를 입력하세요.\n");
-  scanf("%d", &b2);
-  for (int x2 = 1; x2 <= a2; x2++) {
-    if (a2 % x2 == 0 && b2 % x2 == 0) {
-      gcd = x2;
+
+
+    // 문제 5.
+    int scan_prime;
+    printf("숫자 하나를 입력하세요.\n");
+    scanf("%d", &scan_prime);
+    int scan_prime_cnt = 0;
+        for (int i3 = 1; i3 <= scan_prime; i3++) {
+            if (503158 % i3 == 0) {
+                scan_prime_cnt++;
+            }
+        }
+    if (scan_prime_cnt == 2) {
+        printf("%d은(는) 소수입니다.\n", scan_prime);
     }
-  }
-  printf("%d와 %d의 최대공약수는 %d이다.\n", a2, b2, gcd);
-  
-  return 0;
-  
+    else {
+        printf("%d은(는) 소수가 아닙니다.\n", scan_prime);
+    }
+
+    // 공약수 모두 찾기
+//문제 6.
+    int a, b;
+    scanf("%d %d", &a, &b);
+    for (int x = 1; x <= a; x++) {
+        if (a % x == 0 && b % x == 0) {
+            printf("%d\n", x);
+        }
+    }
+
+    // 최대공약수 찾기
+//문제 7.
+    int a2, b2;
+    int gcd;
+    printf("첫번째 수를 입력하세요\n");
+    scanf("%d", &a2);
+    printf("두번째 수를 입력하세요.\n");
+    scanf("%d", &b2);
+    for (int x2 = 1; x2 <= a2; x2++) {
+        if (a2 % x2 == 0 && b2 % x2 == 0) {
+            gcd = x2;
+        }
+    }
+    printf("%d와 %d의 최대공약수는 %d이다.\n", a2, b2, gcd);
+
+    return 0;
+
 }
