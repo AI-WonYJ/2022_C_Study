64fbdd4415a3c14ab026c666e8bd6ae051d8dc7b
AI-WonYJ <101448204+AI-WonYJ@users.noreply.github.com>
2022-07-20 12:23:33 +0900

    Create 6주차_다양한 변수형.C

---
+++ 6주차_다양한 변수형.C
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+            // float 실수 자료형
+  // 문제 1.
+  float weight, height;
+  printf("당신의 몸무게를 입력하세요. (kg 단위)\n");
+  scanf("%f", &weight);
+  printf("당신의 키를 입력하세요. (m 단위)\n");
+  scanf("%f", &height);
+  float BMI = weight / (height * height);
+  printf("당신의 BMI지수는 %f 입니다.\n", BMI);
+  
+  // 문제 2.
+  
+  
+  // 문제 3.
+  
+  
+  // 문제 4.
+  
+            
+  // 문제 5.
+  
+  
+  // 문제 6.
+  
+  
+  return 0;
+}
