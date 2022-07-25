9c71a575e44e689a82f5ba75426c5e390700c102
AI-WonYJ <101448204+AI-WonYJ@users.noreply.github.com>
2022-07-25 14:29:32 +0900

    Update 8주차_함수.C

--- 8주차_함수.C
+++ 8주차_함수.C
@@ -5,13 +5,14 @@
   // 문제 1.
 int hap(int n_1) {
   int cnt_1 = 0;
-  for(int i = 1; i <= n_2; i++) {
+  for(int i = 1; i <= n_1; i++) {
       cnt_1 += i;
   }
   return cnt_1;
 }
+
   // 문제 2.
-int arithmetic(int n_2) {
+int arithmetic(int scan_2) {
   if(scan_2 == 1) {
       printf("덧셈을 선택하셨습니다.\n");
   }
@@ -30,16 +31,16 @@ int arithmetic(int n_2) {
   scanf("%d", &scan_2_1);
   printf("두번째 수를 입력하세요\n");
   scanf("%d", &scan_2_2);
-  if (n_2 == 1) {
+  if (scan_2 == 1) {
       cal_2 = scan_2_1 + scan_2_2;
   }
-  else if (n_2 == 2) {
+  else if (scan_2 == 2) {
       cal_2 = scan_2_1 - scan_2_2;
   }
-  else if (n_2 == 3) {
+  else if (scan_2 == 3) {
       cal_2 = scan_2_1 * scan_2_2;
   }
-  else if (n_2 == 4) {
+  else if (scan_2 == 4) {
       cal_2 = scan_2_1 / scan_2_2;
   }
   return cal_2;
@@ -47,8 +48,75 @@ int arithmetic(int n_2) {
 
   // 문제 3.
 void hello() {
-    printf("안녕하세요\n");
+  printf("안녕하세요\n");
 }
+
+// 문제 4.
+void plus10() {
+    int scan_4;
+    printf("변수를 입력하세요.\n");
+    scanf("%d", &scan_4);
+    printf("%d에 10을 더한 결과는 %d 입니다.\n", scan_4, scan_4 + 10);
+}
+
+  // 문제 5.
+void hello_5() {
+    for(int i_5 = 1; i_5 <= 10; i_5++) {
+        printf("안녕하세요\n");
+    }
+}  
+
+  // 문제 6.
+int big() {
+    int scan_6_1, scan_6_2;
+    int bigger = 0;
+    printf("첫 번째 수를 입력하세요.\n");
+    scanf("%d", &scan_6_1);
+    printf("두 번째 수를 입력하세요.\n");
+    scanf("%d", &scan_6_2);
+    if (scan_6_1 > scan_6_2) {
+        bigger = scan_6_1;
+    }
+    else if (scan_6_2 > scan_6_1) {
+        bigger = scan_6_2;
+    }
+    return bigger
+    }
+
+  // 문제 7.
+void sniffling() {
+    int scan_7;
+    printf("정수 하나를 입력하세요.\n");
+    scanf("%d", &scan_7);
+    if (scan_7 % 2 == 0) {
+        printf("even\n");
+    }
+    else if (scan_7 % 2 != 0) {
+        printf("odd\n");
+    }
+    }
+
+  // 문제 8.
+int sniffling_8() {
+    int scan_8;
+    int sinffl = 0;
+    printf("정수 하나를 입력하세요.\n");
+    scanf("%d", &scan_8);
+    if (scan_8 % 2 == 0) {
+        sinffl = 0;
+    }
+    else if (scan_8 % 2 != 0) {
+        sinffl = 1;
+    }
+    return sinffl;
+    }
+
+
+
+
+
+
+
 
 
 int main() {
@@ -70,6 +138,21 @@ int main() {
   // 문제 3.
   hello();
   hello();
+  
+  // 문제 4.
+  plus10();
+  
+  // 문제 5.
+  hello_5();
+  
+  // 문제 6.
+  pritnf("%d", big());
+  
+  // 문제 7.
+  sniffling();
+  
+  // 문제 8.
+  printf("%d\n",sniffling_8());
   
   return 0;
 }
